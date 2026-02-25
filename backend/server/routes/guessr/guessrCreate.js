0e08180c0bdfd36ecaf97f7a49fe2553fbcec4bb
jkling773 <s0395343@salemstate.edu>
2026-02-25 15:07:32 -0500

    edited routes for guessrcreate and guessrget

--- backend/server/routes/guessr/guessrCreate.js
+++ backend/server/routes/guessr/guessrCreate.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const newUserModel = require('../models/userModel')
+const newUserModel = require('../guessr/users')
 
 router.post('/guesserCreate', async (req, res) => {
 
-    
+    const username = req.body.username;
+    const score = req.body.score;
 
-  })
+    try {
+        const newscore = new Score({
+          user:username,
+          value:score
+        });
 
+        await newscore.save(); 
+        res.status(201).json({ message: 'Score saved successfully' });
+      } catch (error) {
+        console.error('Error saving score:', error);
+        res.status(500).json({ message: 'Failed to save score' });
+  } 
+})
   module.exports = router;
--- backend/server/routes/guessr/guessrGet.js
+++ backend/server/routes/guessr/guessrGet.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const newUserModel = require('../models/userModel')
+const newUserModel = require('../models/userModel');
+const { use } = require("react");
 
-router.post('/guessrGet', async (req, res) => {
+router.get('/users/:username/scores', async (req, res) => {
 
-    
+    const username = req.params.username
 
+    try {
+        const scores = await Score.find({ user: username })
+                                    .sort({ value: -1 }).limit(10);
+                                    res.json(scores);
+        } catch (error) {
+        console.error('Could not get scores:', error);
+    }
   })
 
   module.exports = router;
