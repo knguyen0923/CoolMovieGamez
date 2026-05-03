bf84b739e719b7464c2cd2957f3ce36cee8e703b
ken <knguyen3159@gmail.com>
2026-05-03 08:23:12 -0400

    more changes to corss-origin requests

--- backend/server/server.js
+++ backend/server/server.js
@@ -71,8 +71,16 @@ app.use(cors({
   credentials: true
 }));
 
-app.options("*", (req, res) => {
-  res.sendStatus(200);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
 });
 
 // BODY PARSING
